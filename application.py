e167a1f9ff720ae0576b104b9785834d38a75aaa
poorna-manikanta <poornamanikantakandiboina@gmail.com>
2025-12-16 10:10:07 +0530

    Fix EB port and Procfile

--- application.py
+++ application.py
@@ -40,6 +40,8 @@ def predict_datapoint():
         print("after Prediction")
         return render_template('home.html',results=results[0])
     
+import os
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0",port =port)        
