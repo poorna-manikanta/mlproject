f03bd01bd858ec7d62824a93dfb5dcbf997db034
poorna-manikanta <poornamanikantakandiboina@gmail.com>
2025-12-16 14:36:35 +0530

    Fix EB gunicorn application

--- application.py
+++ application.py
@@ -5,9 +5,12 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
+application = app
+
 app=Flask(__name__)
 
 ## Route for a home page
+
 @app.route('/')
 def index():
     return render_template('index.html') 
@@ -20,12 +23,12 @@ def predict_datapoint():
     else:
         data=CustomData(
             gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
+            race_ethnicity=request.form.get('race_ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
 
         )
         pred_df=data.get_data_as_data_frame()
@@ -37,4 +40,6 @@ def predict_datapoint():
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
         return render_template('home.html',results=results[0])
+
+
     
